ba2dffa53d58f8e5784549776e6e0b6ef9c79eb7
Vikash Mishra <workpurpose@gmail.com>
2024-01-15 14:54:23 +0530

    added new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,16 +6,30 @@ import { WebRequestService } from './web-request.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { NewListComponent } from './pages/new-list/new-list.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: TaskViewComponent
+        redirectTo: 'lists',
+        pathMatch: 'full'
     },
     {
         path: 'new-list',
         component: NewListComponent
-    }
+    },
+    {
+        path: 'lists',
+        component: TaskViewComponent
+    },
+    {
+        path: 'lists/:listId',
+        component: TaskViewComponent
+    },
+    {
+        path: 'lists/:listId/new-task',
+        component: NewTaskComponent
+    },
 ]
 
 // @NgModule({
